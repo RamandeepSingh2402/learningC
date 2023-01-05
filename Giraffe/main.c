79a7aaad8eb317226613452b8c9a0d67ab4bb5be
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-05 18:04:16 -0400

    variables in C

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("   /|\n");
-    printf("  / |\n");
-    printf(" /  |\n");
-    printf("/___|\n");
+    char characterName[] = "John";
+    int characterAge = 35;
+    printf("There once was a man named %s\n", characterName);
+    printf("He was %d years old. \n", characterAge);
+
+    characterAge = 30;
+    
+    printf("He really liked the name %s\n", characterName);
+    printf("But did not like being %d.\n", characterAge);
     return 0;
 }
