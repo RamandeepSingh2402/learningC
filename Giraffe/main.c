9321b7d9fcbfd6f98671821b5a12da7743a5ef63
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-05 18:26:54 -0400

    used more printf "

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
 int main() {
-
-    int age = 10;
-    double gpa = 3.6;
-    char grade = 'A';
-    char phrase[] = "Giraffe Academy";
-
+    printf("Hello \"World\"");
     return 0;
 }
