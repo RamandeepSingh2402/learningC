da46ec5c8c916037d3ec30b65a0e2f21c41a1990
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-05 18:49:14 -0400

    learnt different data types, Math functions and comments in C

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    printf("Hello \"World\"");
+
+    printf("Two raised to a power of 3 is: %f\n", pow(2,3));
+    printf("Square root of 36 is %f\n", sqrt(36));
+    printf("The ceiling of 36.3 is %f\n", ceil(36.3));
+    printf("The floor of 36.656 is %f\n", floor(36.656));
+    printf("Comments are fun");
+    // This is a single line comment
+    /*
+      This is a multiline comment in C
+    */
     return 0;
 }
