944d12b565673c6ead9379028cbcf2e1f2f0d46c
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-06 05:53:12 -0400

    made a basic Mad Libs Game

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -2,15 +2,32 @@
 #include <math.h>
 
 int main() {
-    double firstNum;
-    double secondNum;
 
-    printf("Enter the first number:");
-    scanf("%lf", &firstNum);
+    char color[20];
+    char pluralNoun[20];
+    char celebrity[20];
+    char celebrityFirstName[20];
+    char celebrityLastName[20];
 
-    printf("Enter the second number:");
-    scanf("%lf", &secondNum);
+    printf("Enter a color: ");
+    scanf("%s", color);
 
-    printf("The sum of the two numbers is: %f", firstNum + secondNum);
+    printf("Enter a plural noun: ");
+    scanf("%s", pluralNoun);
+
+    while((getchar())!='\n');
+
+    printf("Enter a celebrity: ");
+    // To get the first name and the last name of the celebrity using two different strings
+    // scanf("%s%s", celebrityFirstName, celebrityLastName);
+    // scanf() stops taking input after that first space in the string
+    // To get the first and last name of the celebrity using the fgets() function
+    fgets(celebrity, 20,stdin);
+
+    printf("Roses are %s\n", color);
+    printf("%s are blue\n", pluralNoun);
+
+    printf("I love %s\n", celebrity);
+
     return 0;
 }
