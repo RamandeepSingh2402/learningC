b3998fc9ceea115d4828824165f810914016aaf0
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-06 06:15:13 -0400

    learnt and practised about arrays in C

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -2,32 +2,14 @@
 #include <math.h>
 
 int main() {
-
-    char color[20];
-    char pluralNoun[20];
-    char celebrity[20];
-    char celebrityFirstName[20];
-    char celebrityLastName[20];
-
-    printf("Enter a color: ");
-    scanf("%s", color);
-
-    printf("Enter a plural noun: ");
-    scanf("%s", pluralNoun);
-
-    while((getchar())!='\n');
-
-    printf("Enter a celebrity: ");
-    // To get the first name and the last name of the celebrity using two different strings
-    // scanf("%s%s", celebrityFirstName, celebrityLastName);
-    // scanf() stops taking input after that first space in the string
-    // To get the first and last name of the celebrity using the fgets() function
-    fgets(celebrity, 20,stdin);
-
-    printf("Roses are %s\n", color);
-    printf("%s are blue\n", pluralNoun);
-
-    printf("I love %s\n", celebrity);
-
+    // Arrays in C
+    // Arrays: DS where we can store a bunch of data values
+    // The string of characters is basically an array of characters
+    char phrase[] = "Hello there";
+    char anotherPhrase[10];
+    int luckyNumbers[10];
+    luckyNumbers[0] = 1;
+    luckyNumbers[1] = 80;
+    printf("%d", luckyNumbers[1]);
     return 0;
 }
