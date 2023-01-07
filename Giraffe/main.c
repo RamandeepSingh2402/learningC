95907ae7b5ffc155cd4a40cbd3350e70ee139c9d
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-07 17:07:12 -0400

    made a better calculator in C

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
 
-int max (int num1, int num2, int num3) {
-    int result;
-    // The AND operator is: &&
-    // The OR operator is: ||
-    if(num1 >= num2 && num1 >= num3) {
-        result = num1;
-    }
-    else if(num2 > num3 && num2 > num3) {
-        result = num2;
-    }
-    else {
-        result = num3;
-    }
-    return result;
-}
 
 int main() {
-    int num1;
-    int num2;
-    int num3;
-    printf("Enter the first integer: \n");
-    scanf("%d", &num1);
-    while((getchar()) != '\n');
-    printf("Enter the second integer: \n");
-    scanf("%d", &num2);
-    printf("Enter the second integer: \n");
-    scanf("%d", &num3);
-    printf("The larger integer is: %d", max(num1, num2, num3));
-    
+    double num1;
+    double num2;
+    char op;
+    printf("Enter the first number: ");
+    scanf("%lf", &num1);
+    printf("Enter the operator: ");
+    scanf(" %c", &op);
+    printf("Enter the second number: ");
+    scanf("%lf", &num2);
+
+
+    if(op == '+') {
+        printf("%f", num1 + num2);
+    }
+    else if(op == '-') {
+        printf("%f", num1 - num2);
+    }
+    else if(op == '/') {
+        printf("%f", num1 / num2);
+    }
+    else if(op == '*') {
+        printf("%f", num1 * num2);
+    }
+    else{
+        printf("Invalid operator.");
+    }
+
+
 
     return 0;
 }
