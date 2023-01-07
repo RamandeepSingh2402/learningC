e4eab8a27cd8a468516d1e0175664d7963ef61d2
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-07 16:57:11 -0400

    learnt using conditionals in C

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
-#include <math.h>
 
-// Return statements in C
-
-double cube(double num);
-
+int max (int num1, int num2, int num3) {
+    int result;
+    // The AND operator is: &&
+    // The OR operator is: ||
+    if(num1 >= num2 && num1 >= num3) {
+        result = num1;
+    }
+    else if(num2 > num3 && num2 > num3) {
+        result = num2;
+    }
+    else {
+        result = num3;
+    }
+    return result;
+}
 
 int main() {
-    double input;
-    double result;
-    printf("Enter the number which you want to be cubed: ");
-    scanf("%lf", &input);
-    result = cube(input);
-    printf("The cubed value is: %f", result);
+    int num1;
+    int num2;
+    int num3;
+    printf("Enter the first integer: \n");
+    scanf("%d", &num1);
+    while((getchar()) != '\n');
+    printf("Enter the second integer: \n");
+    scanf("%d", &num2);
+    printf("Enter the second integer: \n");
+    scanf("%d", &num3);
+    printf("The larger integer is: %d", max(num1, num2, num3));
+    
+
     return 0;
 }
 
-double cube(double num) {
-    return num * num * num;;
-}
