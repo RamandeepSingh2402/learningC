4027755c24caf44e6511f05fb9861b471b8f7ae2
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-08 07:45:37 -0400

    learnt about structs in C

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 
+struct Student {
+    char name[50];
+    char major[50];
+    int age;
+    double gpa;
+
+};
 
 int main() {
-    char grade = 'A';
+    struct Student student1;
+    student1.age = 20;
+    student1.gpa = 3.60;
+    // Using string copy
+    strcpy(student1.name, "Ramandeep");
+    strcpy(student1.major, "Computer Science");
 
-    // Switch statements in C
-    switch (grade) {
-        case 'A':
-            printf("Your grade is A.");
-            break;
-        case 'B':
-            printf("You grade is B.");
-            break;
-        case 'C':
-            printf("Your grade is C.");
-            break;
-        case 'D':
-            printf("Your grade is D.");
-            break;
-        case 'E':
-            printf("Your grade is E.");
-            break;
-        case 'F':
-            printf("Your grade is F.");
-            break;
+    struct Student student2;
+    student2.age = 20;
+    student2.gpa = 2.5;
+    // Using string copy
+    strcpy(student2.name, "Jim");
+    strcpy(student2.major, "Arts");
 
-        default:
-            printf("Invalid grade.");
-            break;
-    }
-
+    printf("The name of the first student is: %s\n",student1.name);
+    printf("The name of the second student is: %s\n",student2.name);
     return 0;
 }
 
