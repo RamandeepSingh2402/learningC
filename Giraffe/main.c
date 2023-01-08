f3076d3d1911f785892a503dcc3a3b3c4ac74733
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-08 07:58:50 -0400

    learnt using while and do while loop in C

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
-#include <string.h>
-
-struct Student {
-    char name[50];
-    char major[50];
-    int age;
-    double gpa;
-
-};
 
 int main() {
-    struct Student student1;
-    student1.age = 20;
-    student1.gpa = 3.60;
-    // Using string copy
-    strcpy(student1.name, "Ramandeep");
-    strcpy(student1.major, "Computer Science");
+    int index1 = 1;
+    int index2 = 1;
+    printf("The results of while loop are: \n");
+    // while loop
+    while(index1 <=5 ) {
+        printf("The index value is: %d\n", index1);
+        index1++;
+    }
 
-    struct Student student2;
-    student2.age = 20;
-    student2.gpa = 2.5;
-    // Using string copy
-    strcpy(student2.name, "Jim");
-    strcpy(student2.major, "Arts");
+    printf("The results of do while loop are: \n");
+    // do while loop
+    do {
+        printf("The index value is: %d\n", index2);
+        index2++;
+    } while (index2 <=5 );
 
-    printf("The name of the first student is: %s\n",student1.name);
-    printf("The name of the second student is: %s\n",student2.name);
+
+
     return 0;
 }
 
