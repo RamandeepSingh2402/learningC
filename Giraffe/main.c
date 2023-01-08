468928cff702f5b784621c9db8346b99cbc9349c
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-08 08:49:06 -0400

    learnt using 2D Arrays and nested for loops in C

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -2,13 +2,32 @@
 
 int main() {
 
-    int luckyNumbers[] = { 2,4,6,8,10 };
+    // 2-D Arrays in C
+    int nums[3][2] = {
 
-    // For loops in C
-    printf("The values in the provided array are as follows: \n");
-    for(int i = 0; i<5; i++){
-        printf("%d\n", luckyNumbers[i]);
+                {1, 2},
+                {3, 4},
+                {5, 6}
+    };
+
+
+    printf("The original value at (0,0) is: %d\n", nums[0][0]);
+    printf("The original value at (0,1) is: %d\n", nums[0][1]);
+    printf("The original value at (1,0) is: %d\n", nums[1][0]);
+    printf("The original value at (1,1) is: %d\n", nums[1][1]);
+    printf("The original value at (2,0) is: %d\n", nums[2][0]);
+    printf("The original value at (2,1) is: %d\n", nums[2][1]);
+
+    printf("Result using nested loops: \n");
+    // Nested for loops
+    int i, j;
+    for(i = 0; i<3; i++) {
+        for(j = 0; j<2; j++) {
+            printf("(%d,%d): %d     ", i,j, nums[i][j]);
+        }
+        printf("\n");
     }
+
 
     return 0;
 }
