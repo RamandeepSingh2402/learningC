76d3e695d4287eac1feed7ce22b4c78a05a1f3f1
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2023-01-11 12:25:02 -0400

    leant about file pointers

--- Giraffe/main.c
+++ Giraffe/main.c
@@ -2,12 +2,18 @@
 
 int main() {
 
-   // Dereferencing pointers in C
-   int age = 30;
-   int *pAge = &age;
-    printf("The memory address of age is: %p\n", &age);
-    printf("The value stored in age is: %d", *pAge);
 
+    // r - read
+    // w - write: create a new file or write over an existing file
+    // a - append: Means appending on a file.
+    FILE * fpointer = fopen("employees.txt", "w");
+
+    /*
+        If we do not give an absolute path or do not specify the directory,
+        the file would be created in the root directory of where the C file is.
+     */
+
+    fclose(fpointer);
     return 0;
 }
 
